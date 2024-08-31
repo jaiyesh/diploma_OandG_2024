3a16acb4f50f1b9187fc8879ea3cac9bda1b3a79
Jaiyesh Chahar <64524945+jaiyesh@users.noreply.github.com>
2024-08-31 13:23:03 +0530

    Add files via upload

---
+++ math_op.py
@@ -0,0 +1,34 @@
+def addition(a,b):
+    """
+    it will do the addition of two numbers
+    INPUT:
+    a,b : two numbers
+
+    RETURN:
+    c = sum of a and b
+
+
+    """
+
+    c = a+b 
+    return c
+
+
+def substraction(a,b):
+    """
+    it will do the substraction of two numbers
+    INPUT:
+    a,b : two numbers
+
+    RETURN:
+    c = difference of a and b
+
+
+    """
+
+    c = a-b 
+    return c
+
+import numpy as np
+
+np.array([43])
